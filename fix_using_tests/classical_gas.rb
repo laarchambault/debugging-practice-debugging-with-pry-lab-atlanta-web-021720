048dd086220efb213d48e2f2fb19963a15387073
Laura Archambault <laarchambault@gmail.com>
2019-11-15 20:51:54 +0000

    Automatically backed up by Learn

--- fix_using_tests/classical_gas.rb
+++ fix_using_tests/classical_gas.rb
@@ -24,4 +24,6 @@ petrol_petes = GasStation.new("Petrol Pete's", 50)
 seashore_shell = GasStation.new("The Seashore Shell", 40)
 dinobones = GasStation.new("Dino Bones Gas and Grill", 60)
 
-def brand=()
+def unleaded_price=(value)
+  @unleaded_price = value
+end
